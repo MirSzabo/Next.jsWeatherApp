3af2fa6ccbcbdf74ba083030709ca4b97e7cebf7
MirSzabo <poradova.m@gmail.com>
2020-07-29 14:30:18 +0200

    fetch the API

--- README.md
+++ README.md
@@ -14,10 +14,16 @@ Install npm dependencies
 ```
 
 ## Running the project
-The project is written in React and Next.js. To start the development server
+The project is written in React and Next.js. To start the production server
 
 ```sh
 npm start
+```
+
+To start development mode
+
+```sh
+npm run dev
 ```
 
 To build for production
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,25 @@
 import Layout from "../components/Layout";
+import fetch from "isomorphic-unfetch";
 
-const WeatherApp = (props) => (
+const capital = "Copenhagen";
+const api = "https://api.openweathermap.org/data/2.5/weather?q=";
+const cityName = capital.toLowerCase();
+const key = process.env.API_KEY;
+const units = "&units=metric";
+const url = api + cityName + key + units;
+
+const WeatherApp = () => (
   <Layout>
     <div>
       <h1>Weather Forecast</h1>
     </div>
   </Layout>
 );
+
+WeatherApp.getInitialProps = async () => {
+  const res = await fetch(url);
+  const weatherData = await res.json();
+  return weatherData;
+};
 
 export default WeatherApp;
