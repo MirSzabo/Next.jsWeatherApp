17fad5c5d0ba421cdc971759b9752e6c359a68f2
MirSzabo <poradova.m@gmail.com>
2020-08-23 23:47:36 +0200

    new feature described

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,14 @@ import Layout from "../components/Layout";
 import Forecast from "../components/Forecast";
 import Details from "../components/Details";
 import Link from "next/link";
+/*Guidelines:
+
+- API endpoint: https://itunes.apple.com/search?term=radiohead
+- Use isomorphic unfetch instead of fetch
+- Use hooks
+- You can use useState or Redux, but Redux (with hooks) is preferred
+- Notice that the routing is given by the names of the pages
+- To filter songs less than 4 minutes use a URL query parameter*/
 
 const cities = ["Amsterdam", "Brusel", "London"];
 
